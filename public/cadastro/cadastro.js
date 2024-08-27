b424778b3413ab6e8726ee79e082d0a1561ea5e3
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2024-08-27 19:11:33 -0300

    cadastrando no banco de dados

--- public/cadastro/cadastro.js
+++ public/cadastro/cadastro.js
@@ -5,8 +5,8 @@ const form = document.getElementById("form-cadastro");
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
 
-  const usuario = form["input-usuario"].value; //propriedades do objeto form
+  const nome = form["input-usuario"].value; //propriedades do objeto form
   const senha = form["input-senha"].value;
 
-  emitirCadastrarUsuario({ usuario, senha });
+  emitirCadastrarUsuario({ nome, senha });
 });
